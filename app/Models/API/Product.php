507d74569a2dcac2f52ba9398d4923a30f172c76
Shade2012 <ubasanino@gmail.com>
2023-10-12 23:53:03 +0700

    20231012-Master

--- app/Models/API/Product.php
+++ app/Models/API/Product.php
@@ -5,6 +5,7 @@ namespace App\Models\API;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
